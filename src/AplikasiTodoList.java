ce1fa5390b4993fa386d6162c564324682fcb754
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-28 09:39:41 +0700

    Test Remove todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -6,7 +6,7 @@ public class AplikasiTodoList {
     public static void main(String[] args) {
      
         
-        testShowTodoList();
+        testRemoveTodoList();
       
 
 
@@ -75,12 +75,68 @@ public class AplikasiTodoList {
         }
 
     }
+
+    public static void testAddTodoList() {
+
+        for(int i = 0; i < 25; i++){
+            addTodoList("Contoh Todo ke ." + i);
+        }
+        showTodoList();
+    }
+
+    
     /**
      * Menghapus todo list
      */
 
-    public static void removeTodoList() {
+    public static boolean removeTodoList(Integer number) {
 
+        // Validasi/pengecekan
+        if((number -1) >= model.length){
+            return false;
+        }
+
+       else if(model[number -1] == null){
+            return false;
+        }
+        else {
+          
+            // Satu
+            // dua
+            // null
+
+            for(int i = (number -1); i < model.length; i++){
+                if(i == (model.length -1)) {
+                    model[i] = null;
+                }  else {
+                     model[i] = model[i+1];
+                }
+           
+            }
+            return true;
+        }
+
+    }
+
+    public static void testRemoveTodoList(){
+        addTodoList("Satu");
+        addTodoList("Dua");
+        addTodoList("Tiga");
+        addTodoList("Empat");
+        addTodoList("Lima");
+        // addTodoList("Empat");
+        // addTodoList("Lima");
+
+        var result = removeTodoList(20);
+        System.out.println(result);
+
+        result = removeTodoList(7);
+        System.out.println(result);
+
+        result = removeTodoList(2);
+        System.out.println(result);
+
+        showTodoList();
     }
 
     /**
